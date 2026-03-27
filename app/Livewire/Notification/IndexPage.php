95766690687d40b26fd1e7fa7fa0c3a0acad6cea
anthonymacs <157227172+anthonymacs@users.noreply.github.com>
2026-03-27 15:27:54 +0700

    newly added feature

---
+++ app/Livewire/Notification/IndexPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Notification;
+
+use Livewire\Component;
+
+class IndexPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.notification.index-page');
+    }
+}
