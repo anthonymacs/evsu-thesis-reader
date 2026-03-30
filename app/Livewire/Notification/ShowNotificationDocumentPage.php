e725f3d6a5656eebe752ee19f53acff4532b6128
anthonymacs <157227172+anthonymacs@users.noreply.github.com>
2026-03-30 12:55:36 +0700

    updated

---
+++ app/Livewire/Notification/ShowNotificationDocumentPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Notification;
+
+use Livewire\Component;
+
+class ShowNotificationDocumentPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.notification.show-notification-document-page');
+    }
+}
